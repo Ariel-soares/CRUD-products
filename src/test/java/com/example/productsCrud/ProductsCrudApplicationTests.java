2b68d80a936e5c1006ee7393e26ca8f3d6e47e6a
Ariel Soares <ariel.sfranco@gmail.com>
2023-12-01 15:44:34 -0300

    inicio do projeto

---
+++ src/test/java/com/example/productsCrud/ProductsCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productsCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
